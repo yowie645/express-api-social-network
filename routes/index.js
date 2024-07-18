8a3a7615b2d3f9c91fbc2a18078a8ba07392e720
minxrka <minorka04@mail.ru>
2024-07-18 11:59:04 +0300

    1.0.1
    mongodb connection via docker + authorization and database launch via prisma.

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const uploadDestination = "uploads";
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const uploads = multer({ storage: storage });
+
+router.get("/register", (req, res) => {
+  res.send("register");
+});
+
+module.exports = router;
