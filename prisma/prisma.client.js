151d1f720199514bfd38f46335923993c7096097
minxrka <minorka04@mail.ru>
2025-05-11 18:56:25 +0300

    *prisma.client

--- prisma/prisma.client.js
+++ prisma/prisma.client.js
@@ -1,5 +1,24 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-module.exports = {
-  prisma,
-};
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+async function checkDatabaseConnection() {
+  try {
+    await prisma.$connect();
+    console.log('✅ Database connected successfully');
+  } catch (error) {
+    console.error('❌ Database connection error:', error);
+    process.exit(1);
+  }
+}
+
+checkDatabaseConnection();
+
+module.exports = { prisma };
